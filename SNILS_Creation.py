fc068e9a174e5e1c41f1676487b6752436d442a7
tiggerrzn <tiggerrzn@gmail.com>
2020-08-10 17:48:12 +0300

    00 instead 0 for control = 100 control = 101 added

--- SNILS_Creation.py
+++ SNILS_Creation.py
@@ -2,17 +2,19 @@ import random
 
 
 lst = [random.randrange(0, 10) for i in range(9)]
+
 sum_chisel = 0
 for i in range(9):
     sum_chisel += lst[i] * (9 - i)
+
 control = 0
 if sum_chisel < 100:
     control = sum_chisel
 elif sum_chisel > 101:
     control = sum_chisel % 101
-    if control == 100:
-        control = 0
-print(sum_chisel)
+    if control == 100 or 101:
+        control = '00'
+
 for i in lst:
     print(i, end='')
 print(control)
